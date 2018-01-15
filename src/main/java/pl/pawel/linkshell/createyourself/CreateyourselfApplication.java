5b925d0d994ec6a80b7ea91e6fdc30c9619295a3
Jan Koszela <jan.koszela@eclipsegroup.co.uk>
2018-01-15 15:49:45 +0100

    Initial commit

---
+++ src/main/java/pl/pawel/linkshell/createyourself/CreateyourselfApplication.java
@@ -0,0 +1,12 @@
+package pl.pawel.linkshell.createyourself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateyourselfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateyourselfApplication.class, args);
+	}
+}
