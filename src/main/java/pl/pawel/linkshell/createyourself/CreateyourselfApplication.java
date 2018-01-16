6e0e35b668ac1e9723ffa528257e4af85cfda132
Jan Koszela <jan.koszela@eclipsegroup.co.uk>
2018-01-16 15:59:40 +0100

    Add showing beans in context

--- src/main/java/pl/pawel/linkshell/createyourself/CreateyourselfApplication.java
+++ src/main/java/pl/pawel/linkshell/createyourself/CreateyourselfApplication.java
@@ -1,12 +1,33 @@
 package pl.pawel.linkshell.createyourself;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import pl.pawel.linkshell.createyourself.model.lifesense.FundamentalDogma;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class CreateyourselfApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CreateyourselfApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("\nLet's inspect the beans provided by Spring Boot:\n");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
 	}
 }
